71b7266829c411eddadadb070c51f2275df28aa6
Methyltheobromine <flow.xela@gmail.com>
2020-04-15 19:52:28 +0200

    Profiles updated

--- pokeMate/src/main/java/uzh/ase/pokemate/config/MongoConfig.java
+++ pokeMate/src/main/java/uzh/ase/pokemate/config/MongoConfig.java
@@ -3,6 +3,7 @@ package uzh.ase.pokemate.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -11,6 +12,7 @@ import com.mongodb.client.MongoClients;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "uzh.ase.pokemate")
+@Profile({"dev","test"})
 public class MongoConfig {
 
 	@Value("${spring.data.mongodb.uri}")
